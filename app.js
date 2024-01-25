8419d5a95704ac4ddbc1d11e815bc63422bf0c0c
ATribbey <alx_036@yahoo.com>
2024-01-25 11:04:13 -0500

    connect to database in app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 const { PORT = 3001 } = process.env;
 
