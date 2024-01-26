4a890bbf5694730434723ec176f537ec9f48c6e1
ATribbey <alx_036@yahoo.com>
2024-01-26 16:57:53 -0500

    further refine comment block

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 /* Steps for Running Server:
 - Use Bash to run mongod.exe using: "C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe" --dbpath="c:\data\db"
-- Use Mongosh to access server
+- Use Mongosh to access server using: "mongodb://127.0.0.1:27017/wtwr_db"
 - Use terminal in project to run server using either of the following:
   - npm run dev
   - npm run start
