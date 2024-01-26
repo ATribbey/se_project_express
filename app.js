52f1e92e1560141f4c198f2cd1d7f9af0aae5977
ATribbey <alx_036@yahoo.com>
2024-01-26 16:56:34 -0500

    refine comment block

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
-// Run MongoDB command for bash: "C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe" --dbpath="c:\data\db"
-/*Steps for Running Server:
-
-Use Bash to run mongod.exe
-
-Use Mongosh to access server
-
-Use terminal in project to run server using either of the following:
-
-npm run dev
-
-npm run start
+/* Steps for Running Server:
+- Use Bash to run mongod.exe using: "C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe" --dbpath="c:\data\db"
+- Use Mongosh to access server
+- Use terminal in project to run server using either of the following:
+  - npm run dev
+  - npm run start
 */
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
