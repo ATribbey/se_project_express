901a1e863e750e91aff5bc68d3564e3f0005fcf3
ATribbey <alx_036@yahoo.com>
2024-01-28 14:32:59 -0500

    adjust location of temporary middleware for owner handling

--- app.js
+++ app.js
@@ -57,12 +57,6 @@ const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
-const routes = require("./routes");
-
-app.use(express.json());
-
-app.use(routes);
-
 // TEMPORARY WORKAROUND FOR OWNER PROPERTY
 app.use((req, res, next) => {
   req.user = {
@@ -70,6 +64,12 @@ app.use((req, res, next) => {
   };
   next();
 });
+
+const routes = require("./routes");
+
+app.use(express.json());
+
+app.use(routes);
 
 const { PORT = 3001 } = process.env;
 
