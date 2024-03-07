12f8f6718ba70d4329b94e7deab177ab04548cc5
ATribbey <alx_036@yahoo.com>
2024-03-07 12:59:27 -0500

    Add unknown error handling to errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,10 @@
 module.exports = (err, req, res, next) => {
   console.error(err);
+  if (!err.statusCode) {
+    const { statusCode = 500, message } = err;
+    return res.status(statusCode).send({
+      message: statusCode === 500 ? "An error occurred on the server" : message,
+    });
+  }
   return res.status(err.statusCode).send({ message: err.message });
 };
